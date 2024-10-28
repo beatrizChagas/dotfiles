2e111555f57ddd8f7f5dfb12cea071ceebbfdac7
Beatriz Chagas <beatriznery.12@gmail.com>
2024-10-28 16:00:01 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+if ! command -v brew &> /dev/null
+then
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  echo "brew installed"
+else
+  echo "brew already installed"
+fi
+
+libs=( curl icu4c mise )
 files=( alias gitconfig hyper.js zshrc )
+
+for lib in ${libs[@]}
+do
+  brew install $lib
+done
+
 for filename in ${files[@]}
 do
   [[ -s $HOME/.$filename ]] && rm $HOME/.$filename
   ln -sf $PWD/$filename ~/.$filename
 done
+
 source ~/.zshrc
